a4121d6934fd53e77b4d8d0b2c24983326a26fc3
rakiasomai <somai.rakia@hotmail.fr>
2021-02-08 14:29:57 +0100

    task0

---
+++ 0x00-TypeScript/README.md
@@ -0,0 +1,2 @@
+0x00-TypeScript
+Holberton school
---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const student_1: Student = {
+    firstName: 'Rakia',
+    lastName: 'Somai',
+    age: 26,
+    location: 'Tunis',
+}
+
+const student_2: Student = {
+    firstName: 'Khalil',
+    lastName: 'Sediri',
+    age: 30,
+    location: 'Ariana',
+}
+
+const studentsList = [student_1, student_2]
