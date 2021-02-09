d02e2d085ad2eec89f8c856d1bcd55da3e7b3eab
rakiasomai <somai.rakia@hotmail.fr>
2021-02-09 20:58:43 +0100

    task5

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -10,7 +10,7 @@ interface TeacherInterface {
     workTeacherTasks(): string;
 }
 
-export class Director implements DirectorInterface {
+class Director implements DirectorInterface {
     workFromHome(): string {
     return "Working from home";
     }
@@ -24,7 +24,7 @@ export class Director implements DirectorInterface {
       }
 }
 
-export class Teacher implements TeacherInterface {
+class Teacher implements TeacherInterface {
     workFromHome(): string {
       return "Cannot work from home";
     }
@@ -36,12 +36,7 @@ export class Teacher implements TeacherInterface {
     }
   }
 
-export function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === "number" && salary < 500) return new Teacher();
-    else
-    return new Director();
-}
-
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee("$500"));
+const createEmployee = (salary: string | number) => {
+    if (Number(salary) < 500) return new Teacher
+    else return new Director;
+  }
