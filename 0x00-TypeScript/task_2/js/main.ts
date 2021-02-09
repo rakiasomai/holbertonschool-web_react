9110e6b1248674442f2a9bd15f83446f7c628f77
rakiasomai <somai.rakia@hotmail.fr>
2021-02-09 22:20:18 +0100

    task6

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -12,27 +12,25 @@ interface TeacherInterface {
 
 export class Director implements DirectorInterface {
     workFromHome(): string {
-    return "Working from home";
+        return "Working from home";
     }
-
     getCoffeeBreak(): string {
-    return "Cannot have a break";
+        return "Cannot have a break";
     }
-
     workDirectorTasks(): string {
         return "Getting to director tasks";
-      }
+    }
 }
 
 export class Teacher implements TeacherInterface {
     workFromHome(): string {
-      return "Cannot work from home";
+        return "Cannot work from home";
     }
     getCoffeeBreak(): string {
-      return "Cannot have a break";
+        return "Cannot have a break";
     }
     workTeacherTasks(): string {
-      return "Getting to work";
+        return "Getting to work";
     }
 }
 
@@ -40,3 +38,20 @@ export function createEmployee(salary: number | string): Teacher | Director {
     if (typeof salary === "number" && salary < 500) return new Teacher
     else return new Director;
 }
+
+export function isDirector(employee: Teacher | Director): employee is Director {
+    return employee.workFromHome() === "Working from home"
+}
+
+export function executeWork(employee: Director | Teacher): string {
+    var feedback;
+  
+    if (isDirector(employee)) {
+      feedback = employee.workDirectorTasks();
+    } else {
+      feedback = employee.workTeacherTasks();
+    }
+  
+    console.log(feedback);
+    return feedback;
+  }
