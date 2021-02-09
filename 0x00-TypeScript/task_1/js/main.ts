3c2d3aa093e90d330bcb76a67411821a1c6b6aea
rakiasomai <somai.rakia@hotmail.fr>
2021-02-09 15:55:59 +0100

    fix task3

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -25,31 +25,36 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (
+const printTeacher: printTeacherFunction = function (
     firstName: string,
     lastName: string
-): string => {
+): string {
     const intial: string = firstName.charAt(0);
     return `${intial}. ${lastName}`;
 };
 
-interface StudentClass {
-    firstName: string;
-    lastName: string;
+interface Student_Class {
     workOnHomework(): string;
     displayName(): string;
 }
+  
+interface Constructor {
+    new (firstName: string, lastName: string): Student_Class;
+}
 
-class StudentClass {
+export class StudentClass implements Student_Class {
+    firstName: string;
+    lastName: string;
+    
     constructor(firstName: string, lastName: string) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    
     workOnHomework(): string {
-        return 'Currently working';
+        return "Currently working";
     }
-
+    
     displayName(): string {
         return this.firstName;
     }
