01db2fc464ae7cf1ff0113acd304f9b96aeaf75c
rakiasomai <somai.rakia@hotmail.fr>
2021-04-14 14:14:36 +0100

    task0

---
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,72 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import PropTypes from 'prop-types';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import CourseList from '../CourseList/CourseList';
+import { getLatesNotification } from '../utils/utils';
+
+
+class App extends React.Component {
+  render() {
+    const listCourses = [
+      {
+        id: 1,
+        name: 'ES6',
+        credit: 60,
+      },
+      {
+        id: 2,
+        name: 'Webpack',
+        credit: 20,
+      },
+      {
+        id: 3,
+        name: 'React',
+        credit: 40
+      }
+    ];
+  
+    const listNotifications = [
+      {
+        id: 1,
+        type: "default",
+        value: "New course available",
+      },
+      {
+        id: 2,
+        type: "urgent",
+        value: "New resume available",
+      },
+      {
+        id: 3,
+        type: "urgent",
+        html: { __html: getLatesNotification() },
+      }
+  ];
+    return (
+      <Fragment>
+        <div className='App'>
+          <div className='Notif-h'>
+        <Notifications listNotifications={listNotifications} />
+          <Header />
+          </div>
+          <div>
+            {this.isLoggedIn === false && <Login />}
+            {this.isLoggedIn === true && <CourseList listCourses={listCourses} />}
+          </div>
+          <Footer />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+  
+
+export default App;
