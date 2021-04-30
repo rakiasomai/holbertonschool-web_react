b084586287589dc0d82df0a1e95b540f7fe19936
rakiasomai <somai.rakia@hotmail.fr>
2021-04-30 18:29:31 +0100

    fix

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,5 +1,14 @@
-import { fromJS, getIn } from 'immutable';
+import { Map } from 'immutable';
 
-export default function accessImmutableObject(object, array) {
-  return getIn(fromJS(object), array, undefined);
-}
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+export const map2 = map.withMutations((item) => {
+  item.set(2, 'Benjamin').set(4, 'Oliver');
+});
